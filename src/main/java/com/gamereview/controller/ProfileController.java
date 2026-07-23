8d65d51ad80cef013de638417d5725351ae8e106
allan rick <205006092+allanrrick@users.noreply.github.com>
2026-07-23 16:35:52 -0300

    feature: add profile login

--- src/main/java/com/gamereview/controller/ProfileController.java
+++ src/main/java/com/gamereview/controller/ProfileController.java
@@ -5,6 +5,7 @@ import com.gamereview.model.Profile;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ProfileController {
@@ -26,5 +27,37 @@ public class ProfileController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public Profile login(String email, String password) {
+
+        String sql = """
+        SELECT id, username, email, password
+        FROM profile
+        WHERE email = ? AND password = ?
+        """;
+
+        try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+            ResultSet result = stmt.executeQuery();
+
+            if (result.next()) {
+
+                Profile profile = new Profile();
+                profile.setId(result.getInt("id"));
+                profile.setUsername(result.getString("username"));
+                profile.setEmail(result.getString("email"));
+                profile.setPassword(result.getString("password"));
+
+                return profile;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
