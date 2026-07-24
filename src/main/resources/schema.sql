4e48f68ef150c8424aac072ee98212b83c1426e8
Rafael Fagundes Oliveira <rafael.fagundes@cwi.com.br>
2026-07-24 15:29:48 -0300

    feat: sql schema and database initializer implementation

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE gamereview;
+
+CREATE TABLE profile (
+                         id SERIAL PRIMARY KEY,
+                         username VARCHAR(100) NOT NULL UNIQUE,
+                         email VARCHAR(100) NOT NULL UNIQUE,
+                         password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE review (
+                        id SERIAL PRIMARY KEY,
+                        game_title VARCHAR(100) NOT NULL,
+                        platform VARCHAR(50),
+                        rating DECIMAL(2,1) NOT NULL,
+                        text TEXT,
+                        review_date DATE NOT NULL,
+                        profile_id INT NOT NULL,
+
+                        CONSTRAINT fk_review_profile
+                            FOREIGN KEY (profile_id)
+                                REFERENCES profile(id)
+                                ON DELETE CASCADE
+);
+
+CREATE TABLE comment (
+                         id SERIAL PRIMARY KEY,
+                         text TEXT NOT NULL,
+                         comment_date DATE NOT NULL,
+                         profile_id INT NOT NULL,
+                         review_id INT NOT NULL,
+
+                         CONSTRAINT fk_comment_profile
+                             FOREIGN KEY (profile_id)
+                                 REFERENCES profile(id)
+                                 ON DELETE CASCADE,
+
+                         CONSTRAINT fk_comment_review
+                             FOREIGN KEY (review_id)
+                                 REFERENCES review(id)
+                                 ON DELETE CASCADE
+);
+
+CREATE TABLE likes (
+                       id SERIAL PRIMARY KEY,
+                       profile_id INT NOT NULL,
+                       review_id INT NOT NULL,
+
+                       CONSTRAINT fk_likes_profile
+                           FOREIGN KEY (profile_id)
+                               REFERENCES profile(id)
+                               ON DELETE CASCADE,
+
+                       CONSTRAINT fk_likes_review
+                           FOREIGN KEY (review_id)
+                               REFERENCES review(id)
+                               ON DELETE CASCADE,
+
+                       CONSTRAINT uk_likes UNIQUE (profile_id, review_id)
+);
