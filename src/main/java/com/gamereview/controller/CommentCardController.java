440635bf62d2b2b044c00537c570a721b3697f8e
Rafael Fagundes Oliveira <rafael.fagundes@cwi.com.br>
2026-07-26 16:53:35 -0300

    feat: date and add comment button on the beginning of the section

--- src/main/java/com/gamereview/controller/CommentCardController.java
+++ src/main/java/com/gamereview/controller/CommentCardController.java
@@ -4,13 +4,22 @@ import com.gamereview.model.Comment;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.time.format.DateTimeFormatter;
+
 public class CommentCardController {
 
     @FXML private Label lblUser;
     @FXML private Label lblText;
+    @FXML private Label lblDate;
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public void setCommentData(Comment comment) {
         lblUser.setText("@" + comment.getUsername());
         lblText.setText(comment.getText());
+
+        if (comment.getCommentDate() != null) {
+            lblDate.setText(comment.getCommentDate().format(DATE_FORMATTER));
+        }
     }
 }
